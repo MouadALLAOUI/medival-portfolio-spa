9f0a43d8457bd5cd99ef862c1e5f6663559f0ffc
MouadALLAOUI <moadallaoui1@gmail.com>
2026-05-22 10:33:58 +0100

    feat: add hero section with chatbot component and medieval styling theme

--- react_medival_portfolio/src/templates/layouts.jsx
+++ react_medival_portfolio/src/templates/layouts.jsx
@@ -1,12 +1,14 @@
-
-import { Outlet } from "react-router-dom";
-import HeaderComponent from "../components/Headers";
-import Footer from "../components/footer";
+import { Outlet } from 'react-router-dom';
+import HeaderComponent from '../components/Headers';
+import Footer from '../components/footer';
+import SettingsModal from '../components/SettingsModal';
+import { useSettings } from '../lib/useSettings';
 
 const Layouts = () => {
+  const { isSettingsOpen, closeSettings } = useSettings();
+
   return (
-    <div className="body-container">
-      <HeaderComponent />
+    <div className="body-container" id="body-container">
       <HeaderComponent />
       <main>
         <Outlet />
